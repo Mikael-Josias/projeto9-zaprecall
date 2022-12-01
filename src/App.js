acf1f9a4afaaf4fa0db543c4619af8e80c72dece
Mikael Josias <mikaeljosias2002@gmail.com>
2022-12-01 11:44:58 -0300

    style: set App.js new style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <StyledApp>
       <GlobalStyle/>
-      olá mundo!
-    </div>
+      <AppContainer>
+        
+      </AppContainer>
+    </StyledApp>
   );
 }
 
-export default App;
+const StyledApp = styled.div`
+  background-color: #FB6B6B;
+
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppContainer = styled.div`
+  margin: auto;
+  width: 375px;
+`;
